434ee8f636931bf4bb5c9ba02e75b1d971309062
minimum16 <74023952+minimum16@users.noreply.github.com>
2021-03-12 15:46:08 +0000

    Add

---
+++ aula3/BWT.py
@@ -0,0 +1,134 @@
+
+class BWT:
+    
+    def __init__(self, seq = "", buildsufarray = False):
+        self.bwt = self.build_bwt(seq, buildsufarray) 
+    
+    def set_bwt(self, bw):
+        self.bwt = bw
+
+    def buildbwt(self, text, buildsufarray = False):
+        ls = []
+        for i in range(len(text)):
+            ls.append(text[i:]+text[:i])
+        ls.sort()
+        res = ''
+        for i in range(len(text)):
+            res += ls[i][len(text)-1]
+        if buildsufarray:
+            self.sa = []
+            for i in range(len(ls)):
+                stpos - ls[i].index('$')
+                self.sa.append(len(text)-stpos-1)
+        return res
+
+    def build_bwt(self, text, buildsufarray = False):
+        #construir a rotacao da sequencia
+        ls = []
+        for i in range(len(text)):
+            ls.append(text[i:] + text[:i])
+        ls.sort()
+        #vai percorrer a lista de strings e vais guardar o ultimo caractere de cada string
+        res = ''
+        for i in range(len(text)):
+            res += ls[i][len(text)-1]
+        return res    
+    
+    def inverse_bwt(self):
+        firstcol = self.get_first_col()
+        res = ""
+        c = "$" 
+        occ = 1
+        for i in range(len(self.bwt)):
+            pos = find_ith_occ(self.bwt, c, occ)
+            c = firstcol[pos]
+            occ = 1
+            k = pos - 1
+            while firstcol[k] == c and k >= 0:
+                occ += 1
+                k -= 1
+            res += c
+        return res        
+ 
+    def get_first_col (self):
+        firstcol = []
+        for c in self.bwt:
+            firstcol.append(c)
+        firstcol.sort()
+        return firstcol
+        
+    def last_to_first(self):
+        res = []
+        firstcol = self.get_first_col()
+        for i in range(len(firstcol)):
+            c = self.bwt[i]
+            ocs = self.bwt[:i].count(c) + 1
+            res.append(find_ith_occ(firstcol, c, ocs))
+        return res
+
+    def bw_matching(self, patt):
+        lf = self.last_to_first()
+        res = []
+        top = 0
+        bottom = len(self.bwt)-1
+        flag = True
+        while flag and top <= bottom:
+            if patt != "":
+                symbol = patt[-1]
+                patt = patt[:-1]
+                lmat = self.bwt[top:(bottom+1)]
+                if symbol in lmat:
+                    topIndex = lmat.index(symbol) + top
+                    bottomIndex = bottom - lmat[::-1].index(symbol)
+                    top = lf[topIndex]
+                    bottom = lf[bottomIndex]
+                else: flag = False
+            else: 
+                for i in range(top, bottom+1): res.append(i)
+                flag = False            
+        return res        
+ 
+    def bw_matching_pos(self, patt):
+        res = []
+        matches = self.bw_matching(patt)
+        for m in matches:
+            res.append(self.sa[m])
+        res.sort()
+        return res
+ 
+# auxiliary
+ 
+def find_ith_occ(l, elem, index):
+    j, k = 0, 0
+    while k < index and j < len(l):
+        if l[j] == elem:
+            k += 1
+            if k == index:
+                return j
+        j += 1
+    return -1 
+
+      
+def test():
+    seq = "TAGACAGAGA$"
+    bw = BWT(seq)
+    print (bw.bwt)
+    print (bw.last_to_first())
+    print (bw.bw_matching("AGA"))
+
+
+def test2():
+    bw = BWT("")
+    bw.set_bwt("ACG$GTAAAAC")
+    print (bw.inverse_bwt())
+
+def test3():
+    seq = "TAGACAGAGA$"
+    bw = BWT(seq, True)
+    print("Suffix array:", bw.sa)
+#    print(bw.bw_matching_pos("AGA"))
+
+test()
+test2()
+#test3()
+
